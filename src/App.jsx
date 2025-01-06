07776bf59ca8cd16aad6d362daa3f6cf1a368772
Harsh Paradkar <contact.harshparadkar@gmail.com>
2025-01-07 01:18:35 +0530

    Template cleanup and removal of boilerplate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import Hero from './components/Hero'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Global Reset</h1>
+      <Hero />
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import ""
+
+const Hero = () => {
+  return (
+    <section className="hero-section">
+      <div className="hero">
+        <div className="hero-content">
+          <h1 className="heading-primary">
+            A healthy meal delivered to your door, every single day
+          </h1>
+          <p className="hero-decription">
+            The smart 365-days-per-year food subscription that will make you eat
+            healthy again. Tailored to your personal tastes and nutritional
+            needs. We have delivered 250,000+ meals last year!
+          </p>
+          <a href="#" className="btn">
+            Start Eating Healthy
+          </a>
+          <a href="#" className="btn">
+            Learn More &darr;
+          </a>
+        </div>
+        <div className="hero-img-container">
+            <img src="" alt="" srcset="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
