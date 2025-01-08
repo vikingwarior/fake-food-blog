578b0f0235ca71a30f087e6dd3f8f91d30f5a177
Harsh Paradkar <contact.harshparadkar@gmail.com>
2025-01-09 02:14:27 +0530

    Hero Section Wireframe

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,4 @@
-import { heroImg } from "../utils/constants";
-
+import { customerIcons, heroImg } from "../utils/constants";
 
 const Hero = () => {
   return (
@@ -12,17 +11,35 @@ const Hero = () => {
           <p className="hero-decription">
             The smart 365-days-per-year food subscription that will make you eat
             healthy again. Tailored to your personal tastes and nutritional
-            needs. We have delivered 250,000+ meals last year!
+            needs.
           </p>
-          <a href="#" className="btn">
+          <a href="#" className="btn btn--main margin-right-sm">
             Start Eating Healthy
           </a>
-          <a href="#" className="btn">
+          <a href="#" className="btn btn--outline">
             Learn More &darr;
           </a>
+          <div className="customer-icons">
+            <div className="customer-icons-container">
+              {customerIcons.map((customerImg, i) => (
+                <img
+                  src={customerImg}
+                  key={`customer-${i}`}
+                  alt={`customer ${i} image`}
+                />
+              ))}
+            </div>
+            <p>
+              <span><u>250,000+</u></span> meals delivered last year!
+            </p>
+          </div>
         </div>
         <div className="hero-img-container">
-            <img src={heroImg} className="hero-img" alt="Women enjoying food, Meals in Bento Boxes, som cuisines stored in bowles" />
+          <img
+            src={heroImg}
+            className="hero-img"
+            alt="Women enjoying food, Meals in Bento Boxes, som cuisines stored in bowles"
+          />
         </div>
       </div>
     </section>
